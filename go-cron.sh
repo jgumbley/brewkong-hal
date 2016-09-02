4c203d966275825d4336b3f560d0334e8cd10a09
Jim Gumbley <jgumbley@thoughtworks.com>
2016-09-02 20:19:15 +0100

    bootstrap go-cron.sh

---
+++ go-cron.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+logger im here dave
--- go.sh
+++ go.sh
@@ -10,7 +10,7 @@ git clone https://github.com/jgumbley/brewkong-hal.git
 
 cd
 crontab -l > .mycron.bak
-echo "* * * * * echo hello > logger" >> .mycron.bak
+echo "* * * * * /opt/brewkong-hal/go-cron.sh" >> .mycron.bak
 crontab .mycron.bak
 
 tail -f /var/log/syslog
