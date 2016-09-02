e6036c0b104b82549646394eda8fec08092b64a2
Jim Gumbley <jgumbley@thoughtworks.com>
2016-09-02 20:16:09 +0100

    try and spawn an active child

--- go.sh
+++ go.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 echo hello dave :P
+echo -------------------------------------
 
 apt -y install ansible git
+
+cd /opt/
+git clone https://github.com/jgumbley/brewkong-hal.git
+
+cd
+crontab -l > .mycron.bak
+echo "* * * * * echo hello > logger" >> .mycron.bak
+crontab .mycron.bak
+
+tail -f /var/log/syslog
+
 
 
